67e2c57e3139cc3ee0b5e9fe98229e2da935cd80
M Banks <montensia1@outlook.com>
2019-06-22 03:14:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,4 @@
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
 # Create your costume_stores migration here
+
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,4 @@
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 # Create your haunted_houses migration here
+
+end
