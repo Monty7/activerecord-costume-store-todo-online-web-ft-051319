804c66905f9e73712669b9f95120524849ef02e6
M Banks <montensia1@outlook.com>
2019-06-22 03:30:02 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,11 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 # Create your haunted_houses migration here
   create_table :haunted_houses do |t|
-    t.
+    t.string :name 
+    t.string :location
+    
+    
+    
     
   end
 end
