e0a163f106c4619e4875c2fd1f9973fee67cd0bc
M Banks <montensia1@outlook.com>
2019-06-22 03:17:34 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,9 +1,9 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
 # Create your costume_stores migration here
+  def change
+    create_table :costume_stores do |t|
+      t.
+  end
+    
 
-  def create_table :costume_stores do |t|
-    
-  end
-    
-  end
 end
